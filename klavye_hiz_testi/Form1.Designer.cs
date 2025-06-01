187c943fe9e398218c2ffc9f1b47f1c5834f3ebd
Koray Erdoğan <erdogankoray650@outlook.com>
2025-06-01 12:03:12 +0300

    Add project files.

---
+++ klavye_hiz_testi/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace klavye_hiz_testi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label_yanlis = new System.Windows.Forms.Label();
+            this.label_dogru = new System.Windows.Forms.Label();
+            this.button_basla = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(498, 188);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "????";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Location = new System.Drawing.Point(313, 249);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(419, 131);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(45, 260);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Doğru--->";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(45, 311);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Yanlış--->";
+            // 
+            // label_yanlis
+            // 
+            this.label_yanlis.AutoSize = true;
+            this.label_yanlis.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_yanlis.Location = new System.Drawing.Point(156, 311);
+            this.label_yanlis.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_yanlis.Name = "label_yanlis";
+            this.label_yanlis.Size = new System.Drawing.Size(48, 25);
+            this.label_yanlis.TabIndex = 4;
+            this.label_yanlis.Text = "????";
+            // 
+            // label_dogru
+            // 
+            this.label_dogru.AutoSize = true;
+            this.label_dogru.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_dogru.Location = new System.Drawing.Point(156, 259);
+            this.label_dogru.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_dogru.Name = "label_dogru";
+            this.label_dogru.Size = new System.Drawing.Size(48, 25);
+            this.label_dogru.TabIndex = 5;
+            this.label_dogru.Text = "????";
+            // 
+            // button_basla
+            // 
+            this.button_basla.Location = new System.Drawing.Point(313, 418);
+            this.button_basla.Name = "button_basla";
+            this.button_basla.Size = new System.Drawing.Size(126, 53);
+            this.button_basla.TabIndex = 7;
+            this.button_basla.Text = "Başlat";
+            this.button_basla.UseVisualStyleBackColor = true;
+            this.button_basla.Click += new System.EventHandler(this.button_basla_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(468, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 53);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Durdur";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1169, 790);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_basla);
+            this.Controls.Add(this.label_dogru);
+            this.Controls.Add(this.label_yanlis);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label_yanlis;
+        private System.Windows.Forms.Label label_dogru;
+        private System.Windows.Forms.Button button_basla;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ klavye_hiz_testi/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace klavye_hiz_testi
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+            
+        
+            Random random_word = new Random();
+            string[] kelimeler = new string[]
+            {
+               "elma", "armut", "masa", "sandalye", "kalem", "defter", "kitap", "telefon", "bilgisayar", "kedi",
+                "köpek", "güneş", "ay", "yıldız", "araba", "yol", "deniz", "dağ", "orman", "çiçek",
+                "fare", "ekran", "priz", "kablo", "duvar", "pencere", "kapı", "halı", "perde", "şemsiye",
+                "eldiven", "ayakkabı", "mont", "tişört", "pantolon", "saat", "bileklik", "cüzdan", "çanta", "gözlük",
+                "bardak", "tabak", "çatal", "kaşık", "tava", "tencere", "dolap", "masaüstü", "televizyon", "müzik",
+                "radyo", "oyun", "kamera", "hoparlör", "kulaklık", "mikrofon", "şehir", "köy", "kasaba", "mahalle",
+                "yolcu", "otobüs", "minibüs", "taksi", "tren", "uçak", "gemi", "liman", "istasyon", "havaalanı",
+                "göl", "nehir", "şelale", "ova", "çöl", "çayır", "dere", "bataklık", "çamur", "kum",
+                "kaya", "mağara", "buz", "kar", "rüzgar", "yağmur", "fırtına", "yıldırım", "sis", "don",
+                "sabah", "öğle", "akşam", "gece", "yarın", "bugün", "dün", "hafta", "ay", "yıl",
+                "ocak", "şubat", "mart", "nisan", "mayıs", "haziran", "temmuz", "ağustos", "eylül", "ekim",
+                "kasım", "aralık", "öğretmen", "öğrenci", "doktor", "hemşire", "mühendis", "polis", "asker", "şoför",
+               "çiftçi", "terzi", "kasap", "fırıncı", "bakkal", "aşçı", "tezgahtar", "bekçi", "müdür", "yazar",
+                "sanatçı", "müzisyen", "oyuncu", "yönetmen", "şair", "gazeteci", "fotoğrafçı", "ressam", "dansçı", "mimar",
+                "bilim", "teknoloji", "tarih", "coğrafya", "matematik", "fizik", "kimya", "biyoloji", "edebiyat", "felsefe",
+                "ahlak", "inanç", "kültür", "gelenek", "töre", "millet", "devlet", "bayrak", "marş", "vatan",
+                "anayasa", "yasa", "hak", "özgürlük", "barış", "savaş", "adalet", "eşitlik", "oy", "seçim",
+                "başkan", "bakan", "meclis", "mahkeme", "hakim", "avukat", "savcı", "polislik", "komutan", "askerlik",
+                "üniversite", "lise", "ortaokul", "ilkokul", "okul", "ders", "sınav", "kitaplık", "kütüphane", "tahta",
+               "kalemlik", "çöp", "masaüstü", "çanta", "klasör", "proje", "sunum", "ödev", "not", "karne",
+                "tatil", "gezmek", "dinlenmek", "uyumak", "koşmak", "yüzmek", "uçmak", "yürümek", "gülmek", "ağlamak",
+                    "bağırmak", "fısıldamak", "konuşmak", "yazmak", "çizmek", "silmek", "boyamak", "kesmek", "yapıştırmak", "açmak",
+                "kapatmak", "yakmak", "söndürmek", "taşımak", "koymak", "almak", "getirmek", "götürmek", "bakmak", "görmek",
+               "duymak", "tatmak", "koklamak", "düşünmek", "anlamak", "bilmek", "unutmak", "hatırlamak", "sevmek", "nefret",
+               "beğenmek", "istemek", "arzulamak", "özlemek", "korkmak", "çekinmek", "sabretmek", "beklemek", "umut", "hayal",
+                "gerçek", "yalan", "doğru", "yanlış", "çözüm", "problem", "soru", "cevap", "sebep", "sonuç",
+                "neden", "amaç", "hedef", "yol", "başarı", "başarısızlık", "çaba", "emek", "gayret", "kararlılık",
+                "sevgi", "saygı", "hoşgörü", "yardım", "paylaşım", "cömertlik", "nezaket", "dürüstlük", "cesaret", "sabır",
+                "hırs", "kıskançlık", "öfke", "sevinç", "mutluluk", "üzüntü", "şaşkınlık", "korku", "panik", "endişe",
+                "yorgunluk", "açlık", "susuzluk", "hastalık", "sağlık", "ilaç", "tedavi", "doktorluk", "ambulans", "hastane",
+                "klinik", "muayene", "aşı", "serum", "kan", "damar", "kalp", "beyin", "akciğer", "karaciğer",
+                "mide", "bağırsak", "kemik", "kas", "sinir", "deri", "göz", "kulak", "burun", "ağız",
+                "diş", "dil", "saç", "tırnak", "el", "ayak", "bacak", "kol", "omuz", "sırt",
+                "bel", "boyun", "vücut", "ten", "renk", "koku", "ses", "ışık", "gölge", "hareket",
+                "durmak", "oturmak", "yatmak", "ayakta", "koşu", "egzersiz", "spor", "futbol", "basketbol", "voleybol",
+                "tenis", "yüzme", "kayak", "güreş", "okçuluk", "binicilik", "atletizm", "boks", "satranç", "bilardo",
+                "gitar", "piyano", "keman", "davul", "flüt", "klarnet", "müzikçi", "beste", "şarkı", "türkü",
+                "halk", "sanat", "şiir", "roman", "hikaye", "masal", "fıkra", "bilmeceler", "tekerleme", "tarihçe"  };
+        int dogru = 0;
+        int yanlis = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Yeni_kelime_getir();
+        }
+
+        private void Yeni_kelime_getir()
+        {
+            string rastgele_kelime = kelimeler[random_word.Next(kelimeler.Length)];
+            label1.Text = rastgele_kelime.ToString();
+        }
+
+        
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            
+            
+            if (e.KeyCode == Keys.Space)
+            {
+                string kullaniciKelime = richTextBox1.Text.Trim();
+                string hedef_kelime = label1.Text.Trim();
+
+
+                if (!string.IsNullOrEmpty(hedef_kelime))
+                {
+                    if (kullaniciKelime == hedef_kelime)
+                    {
+                        dogru += 1;
+                        label_dogru.Text = dogru.ToString();
+                    }
+
+
+                    else
+                    {
+                        yanlis += 1;
+                        label_yanlis.Text = yanlis.ToString();
+                    }
+                }   
+
+
+                richTextBox1.Clear();
+                Yeni_kelime_getir();
+
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_basla_Click(object sender, EventArgs e)
+        {
+            dogru = 0;
+            yanlis = 0;
+            label_dogru.Text = "0";
+            label_yanlis.Text = "0";
+            richTextBox1.Text = "";
+            Yeni_kelime_getir();
+            richTextBox1.Enabled = true;
+            richTextBox1.Focus();
+            button_basla.Enabled = false;
+            button2.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox1.Enabled=false;
+            button2.Enabled = false;
+            button_basla.Enabled=true;
+            
+        }
+    }
+}
